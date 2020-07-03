7d348b47979c8aa26485ba14fa505fcbf8c71b9c
Jannat-Randhawa <58931852+Jannat-Randhawa@users.noreply.github.com>
2020-07-02 21:31:49 -0400

    settingaxis's

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -26,5 +26,59 @@ var charGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`); 
 
 // Set Initial Params 
-var chosenXAxis = "Poverty"
-    
+var chosenXAxis = "Poverty";
+var chosenYAxis = "healthcare";
+
+// function used for updating x-scale var upon click on the x-axis label 
+function xScale(censusData, chosenXAxis) {
+  // create scales
+  var xLinearScale = d3.scaleLinear()
+    .domain([d3.min(censusData, d => d[chosenXAxis]) * 0.8,
+      d3.max(censusData, d => d[chosenXAxis]) * 1.2
+    ])
+    .range([0, width]);
+
+  return xLinearScale;
+}
+
+function yScale(censusData, chosenXAxis) {
+  // create scales
+  var yLinearScale = d3.scaleLinear()
+    .domain([d3.min(censusData, d => d[chosenXAxis]) * 0.8,
+      d3.max(censusData, d => d[chosenXAxis]) * 1.2
+    ])
+    .range([0, width]);
+
+  return yLinearScale;
+
+}
+
+function renderAxes(newXScale, xAxis) {
+  var bottomAxis = d3.axisBottom(newXScale);
+
+  xAxis.transition()
+    .duration(1000)
+    .call(bottomAxis);
+
+  return xAxis;
+}
+
+function renderAxes(newXScale, xAxis) {
+  var bottomAxis = d3.axisBottom(newXScale);
+
+  xAxis.transition()
+    .duration(1000)
+    .call(bottomAxis);
+
+  return xAxis;
+}
+
+function renderAxes(newYScale, yAxis) {
+  var leftAxis = d3.axisLeft(newYScale);
+
+  xAxis.transition()
+    .duration(1000)
+    .call(leftAxis);
+
+  return yAxis;
+}
