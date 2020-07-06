818463569720a9de1acec652b5ca02a6584c89be
Jannat-Randhawa <58931852+Jannat-Randhawa@users.noreply.github.com>
2020-07-06 14:57:26 -0400

    circle rendered to data

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -59,7 +59,7 @@ function renderXAxis(newXScale, xAxis) {
   var bottomAxis = d3.axisBottom(newXScale);
 
   xAxis.transition()
-    .duration(1000)
+    .duration(2000)
     .call(bottomAxis);
 
   return xAxis;
@@ -173,11 +173,12 @@ d3.csv("./assets/data/data.csv").then(function(acsData){
   var xAxis = chartGroup.append("g")
     .classed("x-axis", true)
     .attr("transform", `translate(0, ${height})`)
-    .call(bottomAxis)
+    .call(bottomAxis);
 
   // append the y axis 
   var yAxis = chartGroup.append("g")
-    .call(leftAxis)
+    .classed("y-axis", true)
+    .call(leftAxis);
 
   var circlesGroup = chartGroup.selectAll("circle")
     .data(acsData)
@@ -261,7 +262,7 @@ d3.csv("./assets/data/data.csv").then(function(acsData){
       .attr("dy", "1em")
       .attr("class", "aText inactive")
       .attr("value", "smokes") 
-      .text("Smokes(%");
+      .text("Smokes(%)");
 
     // updateToolTip function above csv import
     var circlesGroup = updateToolTip(chosenXAxis, chosenYAxis, circlesGroup);
@@ -276,7 +277,7 @@ d3.csv("./assets/data/data.csv").then(function(acsData){
         // replaces chosenXAxis with value
         chosenXAxis = value;
 
-        // console.log(chosenXAxis)
+        console.log(chosenXAxis)
 
         // functions here found above csv import
         // updates x scale for new data
@@ -286,10 +287,30 @@ d3.csv("./assets/data/data.csv").then(function(acsData){
         xAxis = renderXAxis(xLinearScale, xAxis);
 
         // updates circles with new x values
-        circlesGroup = renderCircles(circlesGroup, xLinearScale, chosenXAxis, yLinearScale, chosenYAxis);
+        // circlesGroup = renderCircles(circlesGroup, xLinearScale, chosenXAxis, yLinearScale, chosenYAxis);
+        d3.selectAll("circle").each(function(){
+          console.log(xLinearScale)
+          d3.select(this)
+              .transition()
+              .attr("cx", d => {
+                console.log(d[chosenXAxis])
+                console.log(xScale(acsData, chosenXAxis)(d[chosenXAxis]));
+                // return xLinearScale(d[chosenXAxis])
+                return xScale(acsData, chosenXAxis)(d[chosenXAxis]);
+              })
+              .duration(2000)  
+        });
+        // var circlesGroup = chartGroup.selectAll("circle")
+        //   .data(acsData)
+        //   .enter()
+        //   .append("circle")
+        //   .attr("class", "stateCircle")
+        //   .attr("cx", d => xLinearScale(d[chosenXAxis]))
+        //   .attr("cy", d => yLinearScale(d[chosenYAxis]))
+        //   .attr("r", 20);
+        // // updates tooltips with new info
+        // circlesGroup = updateToolTip(chosenXAxis, chosenYAxis, circlesGroup);
 
-        // updates tooltips with new info
-        circlesGroup = updateToolTip(chosenXAxis, chosenYAxis, circlesGroup);
 
         textGroup = renderText(textGroup, xLinearScale, chosenXAxis, yLinearScale, chosenYAxis);
 
@@ -349,7 +370,18 @@ d3.csv("./assets/data/data.csv").then(function(acsData){
         yAxis = renderYAxis(yLinearScale, yAxis);
 
       // updates circles with new values
-        circlesGroup = renderCircles(circlesGroup, xLinearScale, chosenXAxis, yLinearScale, chosenYAxis);
+      d3.selectAll("circle").each(function(){
+        console.log(yLinearScale)
+        d3.select(this)
+            .transition()
+            .attr("cy", d => {
+              console.log(d[chosenYAxis])
+              console.log(yScale(acsData, chosenYAxis)(d[chosenYAxis]));
+              // return xLinearScale(d[chosenXAxis])
+              return yScale(acsData, chosenYAxis)(d[chosenYAxis]);
+            })
+            .duration(2000)  
+      });
 
       // updates tooltips with new info
         circlesGroup = updateToolTip(chosenXAxis, chosenYAxis, circlesGroup);
